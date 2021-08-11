4935c2a36dd388cb9dee03a46702ba3fcfae6e70
Digitman27 <digitman27@gmail.com>
2021-08-11 23:58:21 +0300

    gui and load tm to gui txt_blok

--- UniversalTM/UniversalTM/MainWindow.xaml.cs
+++ UniversalTM/UniversalTM/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +23,47 @@ namespace UniversalTM
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Thread loadTM_t;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown(0);
+        }
+
+        private void LoadTM(object sender, RoutedEventArgs e)
+        {
+            string filepath = string.Empty;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "uTM files (*.uTM)|*.uTM";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+            Nullable<bool> result = openFileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                //Get the path of specified file
+                filepath = openFileDialog.FileName;
+                //MessageBox.Show("file path", filePath);
+            }
+            if (!filepath.Equals(""))
+            {
+                var reader = new StreamReader(filepath);
+                int line_n = 1;
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    this.TM_code.Text += line_n+".  "+line+"\n";
+                    line_n += 1;
+                }
+
+            }
         }
     }
 }
