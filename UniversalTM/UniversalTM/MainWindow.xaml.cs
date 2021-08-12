474b9b83bfba57656d700b51aa06ca62a7721630
Digitman27 <digitman27@gmail.com>
2021-08-12 22:30:42 +0300

    control flow check for the TM load + error detection for the TM load + gui change

--- UniversalTM/UniversalTM/MainWindow.xaml.cs
+++ UniversalTM/UniversalTM/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,6 +27,9 @@ namespace UniversalTM
     public partial class MainWindow : Window
     {
         private Thread loadTM_t;
+
+        private Tuple<List<State>, Flags,int> states;
+        private TMclass turingMachine;
         public MainWindow()
         {
             InitializeComponent();
@@ -54,15 +60,66 @@ namespace UniversalTM
             if (!filepath.Equals(""))
             {
                 var reader = new StreamReader(filepath);
-                int line_n = 1;
+                int line_n = 0;
 
                 while (!reader.EndOfStream)
                 {
                     var line = reader.ReadLine();
-                    this.TM_code.Text += line_n+".  "+line+"\n";
+                    this.TM_code.Inlines.Add(new Run(line_n + ".  " + line + "\n"));
                     line_n += 1;
                 }
+                turingMachine = new TMclass(filepath);
+                states = turingMachine.TM();
+                if (states.Item2 == Flags.SUCCESS)
+                {
+                    this.log.Text = "[+] Loading Turing Machine : Success .\n";
+                    this.clearTM.IsEnabled = true;
+                    this.loadTM.IsEnabled = false;
+                    this.insertToTape.IsEnabled = true;
+                    this.input_box.IsEnabled = true;
+                }else if(states.Item2 == Flags.NO_STATES)
+                {
+                    this.TM_code.Inlines.ElementAt(0).Foreground = Brushes.Red;
+                    this.log.Text = "[+] Loading Turing Machine : Failure(States Error) .\n";
+                }
+                else if(states.Item2 == Flags.NO_DESCRIPTION)
+                {
+                    this.TM_code.Inlines.ElementAt(states.Item3).Foreground = Brushes.Red;
+                    this.log.Text = "[+] Loading Turing Machine : Failure(Machine Description Error line: "+states.Item3+"  ) .\n";
+                }
 
+            }
+        }
+
+        private void Write2Tape(object sender, RoutedEventArgs e)
+        {
+            char[] input = this.input_box.Text.ToCharArray();
+            if(input.Length==0)
+            {
+                this.log.Text = "[+] Input is Empty\n";
+            }
+            else
+            {
+                DataTable table = new DataTable();
+                
+                DataRow row = table.NewRow();
+                for (int i = 0; i < input.Length; i++)
+                {
+                    /*var ind_col = new DataGridTextColumn
+                    {
+                        Header = i.ToString(),
+                        Binding = new Binding(input[i].ToString())
+                    };*/
+                    
+                    table.Columns.Add(new DataColumn(i.ToString(),typeof(string)));
+                    
+                    row[i] = input[i].ToString();
+                    
+                }
+                
+                table.Rows.Add(row);
+                this.tape_data.ItemsSource = table.DefaultView;
+                /*this.tape_data.ItemsSource = table.AsDataView();*/
             }
         }
     }
---
+++ UniversalTM/UniversalTM/TMclass.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UniversalTM
+{
+    public enum Flags
+    {
+        NO_STATES = 1,
+        NO_DESCRIPTION = 2,
+        SUCCESS=0
+    }
+    class TMclass
+    {
+        private string filename;
+        public TMclass(string filename)
+        {
+            this.filename = filename;
+        }
+
+        public Tuple<List<State>,Flags,int> TM()
+        {
+            string[] lines = System.IO.File.ReadAllLines(this.filename);
+            List<State> states = new List<State>();
+            int line_count = -1;
+            try 
+            {
+                string s = lines[0];
+                string pattern = "(:)";
+                string[] ss = Regex.Split(s, pattern);
+                string[] ss1 = Regex.Split(ss[2], ",");
+                foreach (string s1 in ss1)
+                {
+                    State state = new State(s1);
+                    state.Name = s1;
+                    states.Add(state);
+                }
+
+            }
+            catch
+            {
+                return Tuple.Create(new List<State>() { }, Flags.NO_STATES, 1);
+            }
+            int ind = 1;
+            if (states.Count != 0)
+            {
+                foreach (string line in lines)
+                {
+                    line_count += 1;
+                    
+                    if (ind == 1 || line == null)
+                    {
+                        ind = 0;
+                        continue;
+                    }
+                    else if (!string.IsNullOrEmpty(line.Trim()))
+                    {
+                        List<string> cu = new List<string>();
+                        System.Console.WriteLine("tokens");
+                        try
+                        {
+                            string[] s_line = Regex.Split(line, "->");
+                            for (int i = 0; i < s_line.Length; i++)
+                            {
+                                string[] tok = Regex.Split(s_line[i], ",");
+                                foreach (string tok_s in tok)
+                                {
+                                    cu.Add(tok_s);
+                                }
+                            }
+                        }catch
+                        {
+                            return Tuple.Create(new List<State>() { }, Flags.NO_DESCRIPTION, line_count);
+                        }
+                        State state = states.Find(o => o.Name.Equals(cu[0]));
+                        System.Console.WriteLine("State: ", state.getName());
+                        string input = cu[1];
+                        if (input == string.Empty)
+                        {
+                            return Tuple.Create(new List<State>() { }, Flags.NO_DESCRIPTION, line_count);
+                        }
+                
+                        if (cu.Count == 4)
+                        {
+                            string move;
+                            State Nextstate;
+                            move = cu[2];
+                            Nextstate = states.Find(o => o.Name == cu[3]);
+                            state.setMove2Tape(input, move);
+                            state.setNextState(input, Nextstate);
+
+                        }
+                        else if (cu.Count == 5)
+                        {
+                            string output, move;
+                            State Nextstate;
+                            output = cu[2];
+                            move = cu[3];
+                            Nextstate = states.Find(o => o.Name == cu[4]);
+                            state.setWrite2Tape(input, output);
+                            state.setMove2Tape(input, move);
+                            state.setNextState(input, Nextstate);
+                        }
+                    }
+                    
+                }
+                
+            }
+
+            return Tuple.Create(states, Flags.SUCCESS, 0);
+        }
+    }
+}
