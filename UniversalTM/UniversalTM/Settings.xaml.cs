fd9c00e0a27e3bacee74fa829dac849133611700
Digitman27 <digitman27@gmail.com>
2021-08-21 22:03:52 +0300

    settings now are saved in .xml file

--- UniversalTM/UniversalTM/Settings.xaml.cs
+++ UniversalTM/UniversalTM/Settings.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Xml;
 
 namespace UniversalTM
 {
@@ -23,7 +25,7 @@ namespace UniversalTM
 
         private void ApplySettings(object sender, RoutedEventArgs e)
         {
-            
+
             ComboBoxItem theme = (ComboBoxItem)this.theme_box.SelectedItem;
             string str_theme = theme.Content.ToString();
             if (!string.IsNullOrEmpty(str_theme))
@@ -53,10 +55,35 @@ namespace UniversalTM
             {
                 inputshowvalue = int.Parse(txt_value);
             }
-            
-            
 
             UpdateLayout();
+
+
+            List<Tuple<string, string, string>> settings = new List<Tuple<string, string, string>>{ 
+                new Tuple<string, string, string>("MainTheme", "Name", str_theme),
+                new Tuple<string, string, string>("InputLimit", "Value", inputshowvalue.ToString()),
+                new Tuple<string, string, string>("Log","Font", execFont.FontFamily.ToString()),
+                new Tuple<string, string, string>("Tape","Font",tapeFont.FontFamily.ToString()),
+                new Tuple<string, string, string>("TM","Font",simTMFont.FontFamily.ToString())
+            };
+
+            XmlDocument config = new XmlDocument();
+            XmlNode decl = config.CreateXmlDeclaration("1.0", "UTF-8", null);
+            XmlElement setting_tag = config.CreateElement("Settings");
+            config.AppendChild(decl);
+            
+            foreach (Tuple<string,string,string> element in settings)
+            {
+                XmlElement xmlEl = config.CreateElement(element.Item1);
+                xmlEl.SetAttribute(element.Item2, element.Item3);
+                setting_tag.AppendChild(xmlEl);
+                
+
+            }
+            config.AppendChild(setting_tag);
+            
+            config.Save("Config.xml");
+
         }
         public int getValue()
         {
